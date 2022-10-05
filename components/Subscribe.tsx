1ce6c2c6807aa1ec65e9f38ea76e77eae6b013a1
EdgarSantiago <edgargsp777@gmail.com>
2022-10-05 13:49:33 -0300

    x

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, ChangeEvent, useState } from 'react';
+import { FormEvent, ChangeEvent, useState } from "react";
 import {
   Stack,
   FormControl,
@@ -9,93 +9,99 @@ import {
   Text,
   Container,
   Flex,
-} from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 
 export default function Simple() {
-  const [email, setEmail] = useState('');
-  const [state, setState] = useState<'initial' | 'submitting' | 'success'>(
-    'initial'
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState<"initial" | "submitting" | "success">(
+    "initial"
   );
   const [error, setError] = useState(false);
 
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
       <Container
-        maxW={'lg'}
-        bg={useColorModeValue('white', 'whiteAlpha.100')}
-        boxShadow={'xl'}
-        rounded={'lg'}
+        maxW={"lg"}
+        bg={useColorModeValue("white", "whiteAlpha.100")}
+        boxShadow={"xl"}
+        rounded={"lg"}
         p={6}
-        direction={'column'}>
+        flexDirection={"column"}
+      >
         <Heading
-          as={'h2'}
-          fontSize={{ base: 'xl', sm: '2xl' }}
-          textAlign={'center'}
-          mb={5}>
+          as={"h2"}
+          fontSize={{ base: "xl", sm: "2xl" }}
+          textAlign={"center"}
+          mb={5}
+        >
           Inscreva-se
         </Heading>
         <Stack
-          direction={{ base: 'column', md: 'row' }}
-          as={'form'}
-          spacing={'12px'}
+          direction={{ base: "column", md: "row" }}
+          as={"form"}
+          spacing={"12px"}
           onSubmit={(e: FormEvent) => {
             e.preventDefault();
             setError(false);
-            setState('submitting');
+            setState("submitting");
 
             // remove this code and implement your submit logic right here
             setTimeout(() => {
-              if (email === 'fail@example.com') {
+              if (email === "fail@example.com") {
                 setError(true);
-                setState('initial');
+                setState("initial");
                 return;
               }
 
-              setState('success');
+              setState("success");
             }, 1000);
-          }}>
+          }}
+        >
           <FormControl>
             <Input
-              variant={'solid'}
+              variant={"solid"}
               borderWidth={1}
-              color={'gray.800'}
+              color={"gray.800"}
               _placeholder={{
-                color: 'gray.400',
+                color: "gray.400",
               }}
-              borderColor={useColorModeValue('gray.300', 'gray.700')}
-              id={'email'}
-              type={'email'}
+              borderColor={useColorModeValue("gray.300", "gray.700")}
+              id={"email"}
+              type={"email"}
               required
-              placeholder={'Seu Email'}
-              aria-label={'Seu Email'}
+              placeholder={"Seu Email"}
+              aria-label={"Seu Email"}
               value={email}
-              disabled={state !== 'initial'}
+              disabled={state !== "initial"}
               onChange={(e: ChangeEvent<HTMLInputElement>) =>
                 setEmail(e.target.value)
               }
             />
           </FormControl>
-          <FormControl w={{ base: '100%', md: '40%' }}>
+          <FormControl w={{ base: "100%", md: "40%" }}>
             <Button
-              colorScheme={state === 'success' ? 'green' : 'gray'}
-              isLoading={state === 'submitting'}
+              colorScheme={state === "success" ? "green" : "gray"}
+              isLoading={state === "submitting"}
               w="100%"
-              type={state === 'success' ? 'button' : 'Enviar'}>
-              {state === 'success' ? <CheckIcon /> : 'Enviar'}
+              type={state === "success" ? "button" : "Enviar"}
+            >
+              {state === "success" ? <CheckIcon /> : "Enviar"}
             </Button>
           </FormControl>
         </Stack>
         <Text
           mt={2}
-          textAlign={'center'}
-          color={error ? 'red.500' : 'gray.500'}>
+          textAlign={"center"}
+          color={error ? "red.500" : "gray.500"}
+        >
           {error
-            ? 'Ocorreu um erro, tente mais tarde novamente!.'
+            ? "Ocorreu um erro, tente mais tarde novamente!."
             : "Você não receberá nenhum spam!"}
         </Text>
       </Container>
